49bf259a203293c31ca38d8c0781e7ce8677044f
serdarselcuk <serdarselcukus@outlook.com>
2019-12-23 00:35:35 -0500

    before formatting '

--- SummerHoliday_java/src/day15_ScannerClass/deneme.java
+++ SummerHoliday_java/src/day15_ScannerClass/deneme.java
@@ -1,5 +1,8 @@
 package day15_ScannerClass;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 public class deneme {
 
@@ -7,5 +10,13 @@ public class deneme {
 	  String str3 = "cybertek";
 		String str4 = new String("cybertek");
 		System.out.println(str3.equals(str4));  // false because of the string pool and heap memory
+		
+	int[] arr = {1,2,3,4,5,6};
+	
+	ArrayList<Integer>list = new ArrayList<>(Arrays.asList(arr));
+	
+	
+	
   }
+
 }
